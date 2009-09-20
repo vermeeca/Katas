5784a1e2843f4211a8fdc9e6acf45508828faccd
Craig Vermeer <craig@vermeers.net>
2009-09-20 14:34:08 -0400

    All nines case pass

--- BowlingKata/BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingKata/BowlingGame.cs
@@ -1,17 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BowlingKata
 {
 	public class BowlingGame
 	{
+		private char[] _balls;
 		public BowlingGame(string game)
 		{
-			throw new NotImplementedException();
+			_balls = game.ToCharArray();
 		}
 
 		public int CalculateScore()
 		{
-			throw new NotImplementedException();
+			int dummy = 0;
+			return (from b in _balls
+			       where Int32.TryParse(b.ToString(), out dummy)
+			       select int.Parse(b.ToString())).Sum();
+
 		}
 	}
 }
