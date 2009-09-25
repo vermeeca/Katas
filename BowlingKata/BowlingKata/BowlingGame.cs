7117731db932e482fda82ced4851c6708f7f1d1a
Craig Vermeer <craig@vermeers.net>
2009-09-24 23:58:11 -0400

    Some tests passing. Calling it a night

--- BowlingKata/BowlingKata/BowlingGame.cs
+++ BowlingKata/BowlingKata/BowlingGame.cs
@@ -7,22 +7,28 @@ namespace BowlingKata
 {
 	public class BowlingGame
 	{
-	    
+	    private List<char> rolled;
+
 		public BowlingGame()
 		{
-			
+		    rolled = new List<char>();
 		}
 
 		public int GetScore()
 		{
-		    return 1;
+		    return rolled.Sum(c => ScoreRoll(c));
 		}
 
-	   
+        public int ScoreRoll(char ball)
+        {
+            return ball == '-' ? 0 : Convert.ToInt32(ball.ToString());
+        }
+
+
 
 	    public void Roll(char ball)
 	    {
-	        
+	        rolled.Add(ball);
 	    }
 	}
 }
--- BowlingKata/BowlingKata/Tests/GameTests.cs
+++ BowlingKata/BowlingKata/Tests/GameTests.cs
@@ -9,24 +9,51 @@ namespace BowlingKata.Tests
 	[TestFixture]
 	public class GameTests
 	{
-		[TestCase("9-9-9-9-9-9-9-9-9-9-", 90)]
+	    private BowlingGame bg;
+
+	    [SetUp]
+	    public void Setup()
+        {
+            bg = new BowlingGame();
+        }
+
+	    [TestCase("9-9-9-9-9-9-9-9-9-9-", 90)]
 		[TestCase("X-X-X-X-X-X-X-X-X-X-XX", 300)]
 		[TestCase("5/5/5/5/5/5/5/5/5/5/5", 150)]
 		public void TestGameScore(string game, int expectedScore)
 		{
-			//int score = new BowlingGame(game).CalculateScore();
-		    var bg = new BowlingGame();
 		    Array.ForEach<char>(game.ToCharArray(), bg.Roll);
-
 			Assert.AreEqual(expectedScore, bg.GetScore());
 		}
 
         [Test]
         public void single_roll_should_be_number()
         {
-            var bg = new BowlingGame();
             bg.Roll('1');
             Assert.AreEqual(1, bg.GetScore());
+        }
+
+        [Test]
+        public void gutterball_should_be_zero()
+        {
+            bg.Roll('-');
+            Assert.AreEqual(0, bg.GetScore());
+        }
+
+        [Test]
+        public void two_rolls_should_add_up()
+        {
+            bg.Roll('1');
+            bg.Roll('1');
+            Assert.AreEqual(2, bg.GetScore());
+        }
+
+        [Test]
+        public void test_just_spare()
+        {
+            bg.Roll('1');
+            bg.Roll('/');
+            Assert.AreEqual(10, bg.GetScore());
         }
 	}
 }
